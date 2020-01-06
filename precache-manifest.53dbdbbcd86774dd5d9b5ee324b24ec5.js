b384bd77370b4ee26c1199aa6046a0d4c235fca7
schadokar <shubhamchadokar04@gmail.com>
2020-01-06 23:28:54 +0530

    Updates

---
+++ precache-manifest.53dbdbbcd86774dd5d9b5ee324b24ec5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f68f0b8f8f3b62b34a90df4427cdf3",
+    "url": "/go-to-do-app/index.html"
+  },
+  {
+    "revision": "30742b46e0f42a2c20b7",
+    "url": "/go-to-do-app/static/css/main.37662559.chunk.css"
+  },
+  {
+    "revision": "6d4b3c24b2f25b485488",
+    "url": "/go-to-do-app/static/js/2.5451f5b4.chunk.js"
+  },
+  {
+    "revision": "30742b46e0f42a2c20b7",
+    "url": "/go-to-do-app/static/js/main.e4bc4fc1.chunk.js"
+  },
+  {
+    "revision": "a1f449e0743e84d5ba8a",
+    "url": "/go-to-do-app/static/js/runtime~main.cfde4ec7.js"
+  }
+]);
